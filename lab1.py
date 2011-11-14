2c3e1eafe682b5c4461faa7a33ddbb4fc960415c
Daynin <golovim@gmail.com>
2011-11-14 22:39:31 +0400

    First and second labs added

---
+++ lab1.py
@@ -0,0 +1,43 @@
+passenter = str(input ('Введите пароль: '))
+
+alf1 = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+alf2 = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+alf3 = ['1','2','3','4','5','6','7','8','9','0']
+alf4 = ['!','@','#','$','%','^','&','*','(',')','_','-','+',':',';','"','?','<','>']
+
+def passlook(password):
+	pl = 0
+	pl1 = 0
+	pl2 = 0
+	pl3 = 0
+	pl4 = 0
+	step = 0
+	while step < len(password):
+		if password[step] in alf1 and pl1 == 0:
+			pl1+=len(alf1)
+		elif password[step]in alf2 and pl2 == 0:
+			pl2+=len(alf2)
+		elif password[step] in alf3 and pl3 == 0:
+			pl3+=len(alf3)
+		elif password[step] in alf4 and pl4 == 0:
+			pl4+=len(alf4)
+		print (pl)
+		pl = pl1 + pl2 + pl3 + pl4
+		step+=1
+	return pl
+	
+def passmeter (passlen):
+	#A =  мощность алфавита
+	#L =  длина пароля
+	V = 100  #скорость перебора паролей злоумышленником
+	T = 12 #время действия пароля
+	P = 10**-4 #вероятность подбора пароля злоумышленником
+	Sn = (V*T)//P  # нижняя граница стойкости
+	Ar = passlook(passenter)
+	print(Ar)
+	L = len (passenter)
+	A = Sn**(1/L)
+	print(Sn, A)
+	ar = passlen + 4
+	print(ar)
+passmeter(passlook)
---
+++ lab2.py
@@ -0,0 +1,16 @@
+import random
+list0 = list('!"#$%&\'()*')
+list1 = list('0123456789')
+list2 = list('qwertyuiopasdfghjklzxcvbnm')
+ID = input('Введите свой идентификатор: ')
+IDl=list(ID)
+count = 0
+password=''
+while count < 8:
+	if count < len(IDl)%5 :
+		password += random.choice(list0)
+	else:
+		password += random.choice(list2)
+	count += 1
+print('Идентификатор пользователя: ', ID)
+print('Сгенерированный пароль    : ', password+(random.choice(list1)), '\n')
